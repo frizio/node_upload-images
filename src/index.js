afa3c84eaa02de80cff80deaae04a2e9f22bd2af
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-01-15 12:44:49 +0100

    Refactor multer configurations

--- src/index.js
+++ src/index.js
@@ -6,30 +6,34 @@ const multer = require('multer');
 // Initialization
 const app = express();
 
+
 // Setting
 app.set('port', 3000);
 const port = app.get('port');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 // Image manager Middleware
-const storage = multer.diskStorage(
+const storage_directory = path.join(__dirname, 'public/uploads');
+
+const storage_config = multer.diskStorage(
     {
-        destination: path.join(__dirname, 'public/uploads'),
+        destination: storage_directory,
         filename: (req, file, cb) => {
             cb(null, file.originalname)
         }
     }
 );
 
-app.use(
-    multer(
-        {
-            storage,
-            dest: path.join(__dirname, 'public/uploads')
-        }
-    ).single('image')
-);
+const multer_config = multer(
+    {
+        storage: storage_config
+    }
+).single('image');
+
+app.use(multer_config);
+
 
 // Routes
 app.get(
@@ -48,6 +52,7 @@ app.post(
         res.send('File uploaded');
     }
 );
+
 
 // Start the server 
 app.listen(
