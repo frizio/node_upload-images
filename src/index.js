49fd0050560e0f6f59a2eb1cae22147850d6a042
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-01-15 10:47:05 +0100

    Create home page and serve them on route /

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+
 
 // Initialization
 const app = express();
@@ -6,7 +8,17 @@ const app = express();
 // Setting
 app.set('port', 3000);
 const port = app.get('port');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// Routes
+app.get(
+    '/',
+    (req, res) => {
+        console.log("Get route /");
+        res.render('index.ejs');
+    }
+);
 
 // Start the server 
 app.listen(
